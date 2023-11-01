1c35da956526a69c23ab046fb8422df9937b7106
Sushank Pandey <sushankpandey233@gmail.com>
2023-11-01 22:08:25 +0545

    imported

--- cmd/web/database.go
+++ cmd/web/database.go
@@ -1,13 +1,15 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
 type databaseConn struct {
 	DB *sql.DB
 }
 
 func (db *databaseConn) create() {
-
+	// hey this is comment
 }
 
 func (db *databaseConn) read() {
