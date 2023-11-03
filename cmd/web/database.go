6e256eca3984b7159a7d1157b14072b79ed28d27
mainali123 <diwashmainali900@gmail.com>
2023-11-03 07:28:00 +0545

    Login signup completed

--- cmd/web/database.go
+++ cmd/web/database.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -25,21 +24,30 @@ func (db *databaseConn) deleteTask() {
 
 }
 
-func (db *databaseConn) createUser(userName string, email string, password string, fName string, lName string, avatarURL string, app *application) int {
-	sqlQuery := "INSERT INTO Users(Username, Email, Password, FirstName, LastName, AvatarURL) VALUES (?, ?, ?, ?, ?, ?)"
+func (db *databaseConn) createUser(userName string, email string, password string, fName string, lName string, avatarURL string, app *application) {
+	sqlQueryInsert := "INSERT INTO Users(Username, Email, Password, FirstName, LastName, AvatarURL) VALUES (?, ?, ?, ?, ?, ?)"
+	//exec, err := db.DB.Exec(sqlQueryInsert, userName, email, password, fName, lName, avatarURL)
+	_, err := db.DB.Exec(sqlQueryInsert, userName, email, password, fName, lName, avatarURL)
 
-	exec, err := db.DB.Exec(sqlQuery, userName, email, password, lName, fName, avatarURL)
 	if err != nil {
 		app.errorLog.Fatal(err.Error())
 	}
-	rowsAffected, err := exec.RowsAffected()
-	if err != nil {
-		app.errorLog.Fatal(err.Error())
-	}
-	fmt.Println(rowsAffected)
-	return int(rowsAffected)
 }
 
 func (db *databaseConn) updateUser() {
 
+}
+
+func (db *databaseConn) loginUser(email string) (string, error) {
+	sqlQuery := "SELECT Password FROM Users WHERE Email=?"
+
+	exec := db.DB.QueryRow(sqlQuery, email)
+
+	var password_ string
+
+	if err := exec.Scan(&password_); err != nil {
+		return "", err
+	} else {
+		return password_, nil
+	}
 }
--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
 	"html/template"
 	"net/http"
-	"time"
 )
 
 // index is the handler for the home page of the website
@@ -20,17 +21,51 @@ func (app *application) index(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// login is the handler for the user loggin in for the first time. It takes care of inserting the user's value in the database
+// login is the handler for the user logging in for the first time. It takes care of inserting the user's value in the database
 func (app *application) login(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("./ui/html/login.html")
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-	err = t.Execute(w, nil)
-	if err != nil {
-		app.serverError(w, err)
-		return
+	if r.Method == http.MethodPost {
+		inputEmail := r.FormValue("email")
+		inputPassword := r.FormValue("password")
+
+		// Check if the email exists
+		doesUserExists := "SELECT Username FROM Users WHERE Email = ?"
+
+		row := app.database.DB.QueryRow(doesUserExists, inputEmail)
+
+		var userExists string
+		err := row.Scan(&userExists)
+
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				// User doesn't exists
+				http.Redirect(w, r, "/signup", 301)
+			} else {
+				// Handle error
+				http.Error(w, "Error while logging in", 500)
+			}
+		} else {
+			// User exists
+			psw, err := app.database.loginUser(inputEmail)
+			if err != nil {
+				app.errorLog.Fatal(err.Error())
+			}
+			if psw == inputPassword {
+				http.Redirect(w, r, "/homepage", 301)
+			} else {
+				http.Error(w, "Wrong password", 500)
+			}
+		}
+	} else {
+		t, err := template.ParseFiles("./ui/html/login.html")
+		if err != nil {
+			app.serverError(w, err)
+			return
+		}
+		err = t.Execute(w, nil)
+		if err != nil {
+			app.serverError(w, err)
+			return
+		}
 	}
 }
 
@@ -44,35 +79,37 @@ func (app *application) signup(w http.ResponseWriter, r *http.Request) {
 		passwordVerification := r.FormValue("password_verification")
 		username := firstname + lastname
 
-		if password != passwordVerification {
+		ifUserExists := "SELECT Username FROM Users WHERE Email = ?"
+		//row := app.database.db.QueryRow(ifUserExists, email)
+		row := app.database.DB.QueryRow(ifUserExists, email)
+
+		var userExists string
+		err := row.Scan(&userExists)
+
+		// if user exists redirect to the login page
+		if err != nil {
+
+			if errors.Is(err, sql.ErrNoRows) {
+				// User doesn't exists
+				if password != passwordVerification {
+					// display error in the browser
+					http.Error(w, "Passwords don't match", 500)
+					return
+				}
+
+				app.database.createUser(username, email, password, firstname, lastname, "./ui/static/avatar/default_avatar.png", app)
+				http.Redirect(w, r, "/login", 301)
+
+			} else {
+				// User exists
+				http.Redirect(w, r, "/login", 301)
+			}
+		} else {
 			// display error in the browser
-			http.Error(w, "Passwords don't match", 500)
+			http.Error(w, "Error while creating user", 500)
 			return
 		}
 
-		val := app.database.createUser(username, email, password, firstname, lastname, "./ui/static/avatar/default_avatar.png", app)
-		if val > 0 {
-			// redirect to the login page
-			http.Redirect(w, r, "/login", 301)
-		} else {
-			ifUserExists := "SELECT * FROM users WHERE email = $1"
-			//row := app.database.db.QueryRow(ifUserExists, email)
-			row := app.database.DB.QueryRow(ifUserExists, email)
-
-			// if user exists redirect to the login page
-			if row != nil {
-				// show info in the browser
-				http.Error(w, "User already exists redirecting to login page", 500)
-				// add delay of 5 seconds
-				time.Sleep(5 * time.Second)
-				http.Redirect(w, r, "/login", 301)
-			} else {
-				// display error in the browser
-				http.Error(w, "Error while creating user", 500)
-				return
-			}
-
-		}
 	} else {
 		t, err := template.ParseFiles("./ui/html/signup.html")
 		if err != nil {
