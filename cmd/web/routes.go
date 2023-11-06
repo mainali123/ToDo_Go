7112132b685b897a161def9adc8491939d70096e
mainali123 <diwashmainali900@gmail.com>
2023-11-06 19:31:39 +0545

    Re-written entire code base till now on Gin framework

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,17 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"github.com/gin-gonic/gin"
+)
 
-func (app *application) routes() *http.ServeMux {
-	mux := http.NewServeMux()
+func (app *application) routes(router *gin.Engine) {
 
-	fileserver := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+	// Define the route handlers
+	router.GET("/", app.index)
+	router.GET("/login", app.login)
+	router.POST("/login", app.login)
+	router.GET("/signup", app.signup)
+	router.POST("/signup", app.signup)
+	router.GET("/homepage", app.homepage)
 
-	mux.HandleFunc("/", app.index)
-	mux.HandleFunc("/login", app.login)
-	mux.HandleFunc("/signup", app.signup)
-	mux.HandleFunc("/homepage", app.homepage)
+	// Define the static file server middleware
+	router.Static("/static", "./ui/static/")
 
-	return mux
 }
